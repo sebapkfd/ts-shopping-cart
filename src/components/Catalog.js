3fc2302ff8558da3465c4c86436b29a675353610
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-17 19:21:04 -0300

    Added display of items on cart

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -4,9 +4,12 @@ import Item from './Item';
 const Catalog = (props) => {
 
     const {items} = props;
+    const amounts = items.map(item => item.amount);
+    const amountInCart = amounts.reduce((acc, cv) => acc + cv);
 
     return(
         <div>
+            <h1>In cart: {amountInCart}</h1>
             {items.map(item =>{
                 return (
                     <Item 
