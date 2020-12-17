219a30b4e6d0ed76c44a50d4187d524842f29610
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-17 19:26:29 -0300

    Added link to repository

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ const Home = () => {
             <Link to='/Catalog'>
                 <button>Start Shopping</button>
             </Link>
+            <a href="https://github.com/sebapkfd/shopping-cart" target="_blank" rel="noreferrer">
+                <img src="https://img.icons8.com/fluent-systems-filled/24/ffffff/github.png" alt="Github"/>
+                <p>Repository</p>
+            </a>
         </div>
     )
 }
