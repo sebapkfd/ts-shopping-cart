1a9ff2792b9f7ad0fc238f7066f657e1a312bac9
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-19 23:40:48 -0300

    Added more assets

--- src/components/listofItems.js
+++ src/components/listofItems.js
@@ -1,26 +1,68 @@
 import uniqid from 'uniqid';
-import Acer from '../assets/Acer-Aspire5.jpg';
-import Asus from '../assets/Asus-TUF.jpg';
-import MSI from '../assets/MSI-GL65.jpg'
+import Aspire from '../assets/Acer-Aspire5.jpg';
+import TUF from '../assets/Asus-TUF.jpg';
+import GL from '../assets/MSI-GL65.jpg'
+import ROG from '../assets/Asus-ROG.jpg'
+import Creator from '../assets/MSI-Creator15.jpg';
+import Blade from '../assets/Razer-Blade.jpg';
+import GE from '../assets/MSI-GE66.jpg';
+import Predator from '../assets/Acer-Predator.jpg';
+import Ultra from '../assets/LG-Ultra.jpg';
 
 const listofItems = [
     {name: 'Acer Aspire 5',
-    price: 100,
+    price: 668,
     id: uniqid(),
     amount: 0,
-    imgSrc: Acer
+    imgSrc: Aspire
     },
     {name: 'Asus TUF',
-    price: 200,
+    price: 749,
     id: uniqid(),
     amount: 0,
-    imgSrc: Asus
+    imgSrc: TUF
     },
     {name: 'MSI GL65',
-    price: 300,
+    price: 1399,
     id: uniqid(),
     amount: 0,
-    imgSrc: MSI
+    imgSrc: GL
     },
+    {name: 'Asus ROG',
+    price: 1499,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: ROG
+    },
+    {name: 'MSI Creator 15',
+    price: 1879,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: Creator
+    },
+    {name: 'Razer Blade 13',
+    price: 1699,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: Blade
+    },
+    {name: 'MSI GE66',
+    price: 2649,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: GE
+    },
+    {name: 'Acer Predator',
+    price: 1629,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: Predator
+    },
+    {name: 'LG Ultra',
+    price: 1279,
+    id: uniqid(),
+    amount: 0,
+    imgSrc: Ultra
+    }
 ]
 export default listofItems;
