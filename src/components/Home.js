8b86592b8bc19400fe3ecfd4cb3367847c8c45ac
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-20 21:12:49 -0300

    Background tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+    
     return(
-        <div>
-            <Link to='/Catalog'>
-                <button className="HomeButton">Start Shopping</button>
-            </Link>
-            <a href="https://github.com/sebapkfd/shopping-cart" target="_blank" rel="noreferrer">
-                <img src="https://img.icons8.com/fluent-systems-filled/24/ffffff/github.png" alt="Github"/>
-                <button className="HomeButton">Repository</button>
-            </a>
+        <div className="HomeDiv">
+                <Link to='/Catalog'>
+                    <button className="HomeButton">Start Shopping</button>
+                </Link>
+                <a href="https://github.com/sebapkfd/shopping-cart" target="_blank" rel="noreferrer">
+                    <button className="HomeButton">
+                        <img src="https://img.icons8.com/fluent-systems-filled/24/ffffff/github.png" alt="Github"/>
+                    </button>
+                </a>
         </div>
     )
 }
