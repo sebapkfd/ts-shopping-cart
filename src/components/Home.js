e479e232f9ae4ca348f9594b38fca0fffbd60967
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-21 00:37:30 -0300

    Basic changes to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,18 @@ const Home = () => {
     
     return(
         <div className="HomeDiv">
+            <div className="HomeInfoDiv">
+                <h1 className="HomeInfo">Find the right Computer</h1>
+                <h1 className="HomeInfo">for you!</h1>
+            </div>
+            <div className="HomeButtonsDiv">
                 <Link to='/Catalog'>
                     <button className="HomeButton">Start Shopping</button>
                 </Link>
                 <a href="https://github.com/sebapkfd/shopping-cart" target="_blank" rel="noreferrer">
                     <button className="HomeButton">Repository</button>
                 </a>
+            </div>
         </div>
     )
 }
