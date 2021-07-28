b1a4a173a419c403a455c48323c7e574c00bd40a
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-28 19:55:30 -0400

    Cleaned unused code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Catalog from "./components/Catalog";
 import Cart from "./components/Cart";
 import ItemPage from "./components/ItemPage";
-import listofItems from "./components/listofItems";
 import "./App.css"
 
 const App = () => {
-  const [items, setItems] = useState(listofItems);
-  const [totalAmount, setTotalAmount] = useState(0);
-  
-  const addSelected = (itemSelected) => {
-    const {name, amount} = itemSelected;
-    const selectedItems = items.map(item =>{
-      if(item.name === name) {
-        return {...item, amount: amount}
-      }
-      return item;
-    })
-    setItems(selectedItems);
-  }
-
-  const cleanCart = () =>{
-    const defaultItems = items.map(item => {
-      return {...item, amount: 0}
-    })
-    setItems(defaultItems);
-  }
-
-  useEffect(() => {
-    const amounts = items.map(item => item.amount);
-    const amountInCart = amounts.reduce((acc, cv) => acc + cv);
-    setTotalAmount(amountInCart);
-  }, [items, totalAmount])
 
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route exact path="/shopping-cart/catalog">
-          <Catalog />
-        </Route>
-        <Route exact path="/shopping-cart/cart">
-          <Cart items={items} onClean={cleanCart}/>
-        </Route>
-        <Route exact path="/shopping-cart/catalog/:id" >
-          <ItemPage />
-        </Route>
-        <Route path="/shopping-cart">
-          <Home />
-        </Route>
+        <Route exact path="/shopping-cart/catalog" component={Catalog} />
+        <Route exact path="/shopping-cart/cart" component={Cart} />
+        <Route exact path="/shopping-cart/catalog/:id" component={ItemPage} />
+        <Route path="/shopping-cart/" component={Home} />
       </Switch>
     </BrowserRouter>
   );
