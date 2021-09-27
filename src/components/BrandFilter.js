30a9ec8008b614d5d5183db59aa5c556104e199e
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-09-27 19:40:18 -0300

    Moved show/hide logic to Brandfilter component

--- src/components/BrandFilter.js
+++ src/components/BrandFilter.js
@@ -1,15 +1,22 @@
 import { itemsBrands } from "../assets/itemsList";
 import { setBrands } from "../redux/filterSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 const BrandFilter = () => {
+    const [showBrands, setShowBrands] = useState(true);
     const checked = useSelector(state => state.filter.brands);
     const dispatch = useDispatch();
 
+    const expandLogo = <span class="material-icons">expand_more</span>
+    const hideLogo = <span class="material-icons">expand_less</span>
 
     return (
         <div>
-            {itemsBrands.map(item => {
+            <div onClick={() => setShowBrands(!showBrands)}>
+                <label>Brands</label>{(showBrands) ? hideLogo : expandLogo}
+            </div>
+            { (showBrands)  ?  (itemsBrands.map(item => {
                 return(
                     <div key={`${item.brand}Opt`} className='brand-option'>
                         <input
@@ -21,7 +28,7 @@ const BrandFilter = () => {
                         <label>{item.brand} ({item.amount})</label>
                     </div>
                 )
-            })}
+            })) : null }
         </div>
     )
 }
