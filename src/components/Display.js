d3f15e97c128ef09aabfdc0a95c5381a29f1b878
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-09-27 19:16:12 -0300

    Added table and list icons

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,10 +4,13 @@ import { setDisplay } from "../redux/displaySlice";
 const Display = () => {
     const dispatch = useDispatch();
 
+    const tableIcon = <span class="material-icons">apps</span>
+    const listIcon = <span class="material-icons">view_list</span>
+
     return(
         <div className='display'>
-            <button onClick={() => dispatch(setDisplay({newDisplay: 'table'}))}>Table</button>
-            <button onClick={() => dispatch(setDisplay({newDisplay: 'list'}))}>List</button>
+            <button onClick={() => dispatch(setDisplay({newDisplay: 'table'}))}>{tableIcon}</button>
+            <button onClick={() => dispatch(setDisplay({newDisplay: 'list'}))}>{listIcon}</button>
         </div>
     )
 }
