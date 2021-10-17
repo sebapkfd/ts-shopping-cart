af09e0deb211a8079c8dd29ea34400992d23cc75
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-17 01:15:12 -0300

    Added Item style module

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styles from '../styles/item.module.css';
 
 const Item = ({ item }) => {
-    const display = useSelector(state => state.display.value);
+    const display = useSelector(state => state.display.value);    
 
     return (
         <Link to={`/shopping-cart/catalog/${item.id}`}>
-            <div className={`${display}-card`}>
-                <div className="card__img">
+            <div className={styles[`${display}Card`]}>
+                <div className={styles[`${display}CardImg`]}>
                     <img src={`${item.imgSrc}`} alt={`${item.name}`} />
                 </div>
-                <div className="card__info">
+                <div className={styles[`${display}CardInfo`]}>
                     <h3>{item.name}</h3>
                     <h3>US$ {item.price}</h3>
                 </div>
