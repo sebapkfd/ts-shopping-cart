3f5b13ce9c0533b94182f0cf561a5cac11500a5e
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-19 22:25:03 -0300

    Improved ItemPage form

--- src/components/AddItemOptions.js
+++ src/components/AddItemOptions.js
@@ -1,8 +1,9 @@
 import AddButton from "./AddButton";
+import styles from '../styles/itemPage.module.css';
 
 const AddItemOptions = ({ item }) => {
     return (item.stock > 0) ? (
-        <div className='add-items-options'>
+        <div className={styles.addOption}>
             <input type="number" name="amount" defaultValue={1} min="1" max={item.stock}/>
             <AddButton id={item.id} />
         </div>
--- src/components/ItemPageForm.js
+++ src/components/ItemPageForm.js
@@ -1,6 +1,5 @@
 import AddItemOptions from './AddItemOptions';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { itemAdded } from '../redux/cartSlice';
 import styles from '../styles/itemPage.module.css';
 
@@ -15,11 +14,6 @@ const ItemPageForm = ({ item }) => {
     return (
         <form className={styles.form} onSubmit={(e) => clickItem(e)}>
             <AddItemOptions item={item} />
-            <Link to='/shopping-cart/catalog'>
-                <button className={styles.backButton}>
-                    Back to catalog
-                </button>
-            </Link>
         </form>
     )
 }
