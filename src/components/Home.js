c61eb6db4eba02187cea79a3d7628452dd8c399f
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-21 00:10:18 -0300

    Added brands section in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/home.module.css';
-import acer from '../assets/thumbnails/home.png';
+import home from '../assets/thumbnails/home.png';
+import acer from '../assets/icons/acer.png';
+import asus from '../assets/icons/asus.png';
+import msi from '../assets/icons/msi.png';
+import razer from '../assets/icons/razer.png';
+import lg from '../assets/icons/lg.png';
 
 const Home = () => {
     return(
         <div className={styles.page}>
             <div className={styles.info}>
                 <div className={styles.thumbnail}>
-                    <img src={acer} alt={'homeImg'}/>
+                    <img src={home} alt={'homeImg'}/>
                 </div>
                 <div className={styles.intro}>
                     <h1>The best technology for you</h1>
@@ -18,10 +23,17 @@ const Home = () => {
                     </Link>
                 </div>
             </div>
-            {/* <div className={styles.brands}>
-                <h1>Brands we work with</h1>
+            <div className={styles.brands}>
+                <h1>Partner Brands</h1>
+                <div>
+                    <img src={acer} alt='acer'/>
+                    <img src={asus} alt='asus'/>
+                    <img src={msi} alt='msi'/>
+                    <img src={razer} alt='razer'/>
+                    <img src={lg} alt='lg'/>
+                </div>
             </div>
-            <div className={styles.aboutUs}>
+            {/* <div className={styles.aboutUs}>
                 <h1>About Us</h1>
             </div> */}
         </div>
