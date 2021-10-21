80727f00f172e69ba8185f39bbe92d53310a5d9b
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-21 17:39:23 -0300

    Removed brands

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/home.module.css';
 import home from '../assets/thumbnails/home.png';
-import acer from '../assets/icons/acer.png';
-import asus from '../assets/icons/asus.png';
-import msi from '../assets/icons/msi.png';
-import razer from '../assets/icons/razer.png';
-import lg from '../assets/icons/lg.png';
 import github from '../assets/icons/github.png';
 import linkedin from '../assets/icons/linkedin.png';
 
@@ -25,17 +20,7 @@ const Home = () => {
                     </Link>
                 </div>
             </div>
-            <div className={styles.brands}>
-                <h1>Partner Brands</h1>
-                <div>
-                    <img src={acer} alt='acer'/>
-                    <img src={asus} alt='asus'/>
-                    <img src={msi} alt='msi'/>
-                    <img src={razer} alt='razer'/>
-                    <img src={lg} alt='lg'/>
-                </div>
-            </div>
-            <div className={styles.aboutUs}>
+            <div className={styles.contact}>
                 <div className={styles.links}>
                     <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
                         <img src={github} alt='github'/>
@@ -44,7 +29,6 @@ const Home = () => {
                         <img src={linkedin} alt='linkedin'/>
                     </a>
                 </div>
-                <h1>1600 Amphitheatre Parkway, Mountain View, California</h1>
             </div>
         </div>
     )
