86e4e0d7bfac5284196ce7290991ba1a50d2dc65
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-21 17:51:37 -0300

    Changes in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/home.module.css';
 import home from '../assets/thumbnails/home.png';
-import github from '../assets/icons/github.png';
-import linkedin from '../assets/icons/linkedin.png';
 
 const Home = () => {
     return(
@@ -18,16 +16,6 @@ const Home = () => {
                     <Link to='/shopping-cart/catalog'>
                         <button>Start Shopping</button>
                     </Link>
-                </div>
-            </div>
-            <div className={styles.contact}>
-                <div className={styles.links}>
-                    <a href='https://github.com/sebapkfd' target='_blank' rel="noopener noreferrer">
-                        <img src={github} alt='github'/>
-                    </a>
-                    <a href='https://www.linkedin.com/in/sebasti%C3%A1n-soto-59b493207/' target='_blank' rel="noopener noreferrer">
-                        <img src={linkedin} alt='linkedin'/>
-                    </a>
                 </div>
             </div>
         </div>
