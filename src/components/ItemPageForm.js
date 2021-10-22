265f25bfdefb961810fe4ba99eee0e3efaae27bc
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-22 16:51:10 -0300

    Changed go back button logic

--- src/components/ItemPageForm.js
+++ src/components/ItemPageForm.js
@@ -1,11 +1,12 @@
 import AddButton from "./AddButton";
 import { useDispatch } from 'react-redux';
 import { itemAdded } from '../redux/cartSlice';
+import { useHistory } from "react-router";
 import styles from '../styles/itemPage.module.css';
-import { Link } from "react-router-dom";
 
 const ItemPageForm = ({ item }) => {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const clickItem = (e) => {
         e.preventDefault();
@@ -16,12 +17,10 @@ const ItemPageForm = ({ item }) => {
         <form className={styles.form} onSubmit={(e) => clickItem(e)}>
             <input type="number" name="amount" defaultValue={1} min="1" max={item.stock}/>
             <AddButton id={item.id} />
-            <Link to='/shopping-cart/catalog'>
-                <button className={styles.backButton}>
-                <span class="material-icons">chevron_left</span>
-                    Back to catalog
+                <button className={styles.backButton} onClick={() =>history.goBack()}>
+                    <span class="material-icons">chevron_left</span>
+                    Back
                 </button>
-            </Link>
         </form>
     );
 }
